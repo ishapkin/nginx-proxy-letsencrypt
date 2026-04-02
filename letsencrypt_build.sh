4d67a8fb647d67a2c27ec331ac76862143deb725
Ilya Shapkin <ilya.shapkin@rtkit.ru>
2026-04-02 14:45:49 +0300

    Remove legacy

--- letsencrypt_build.sh
+++
@@ -1,5 +0,0 @@
-docker run --detach \
-    --name nginx-proxy-letsencrypt \
-    --volumes-from nginx-proxy \
-    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    jrcs/letsencrypt-nginx-proxy-companion
